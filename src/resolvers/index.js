784066de501d9d8ccdc305aa0d8cdd7d5e767968
Hebert Hernandez <hebert.hernandez7@gmail.com>
2021-09-01 23:33:07 -0500

    feat: add graphql resolver

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -3,6 +3,12 @@ const resolvers = {
     hello: () => 'hello, world!',
     greet: (_, { name }) => `Hello ${name}`,
   },
+
+  Mutation: {
+    createBook: (_, { book }) => {
+      console.log(_, book)
+    },
+  },
 }
 
 module.exports = { resolvers }
